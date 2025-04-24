8da24017a45d7efe691591258749128762269956
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-04-24 12:35:18 -0700

    starting process of building phoneme selection menu

---
+++ src/prefabs/phoneme_assembler/phoneme_assembler.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const TOTAL_PHONEMES : int = 44
+
+@export var container_to_fill : Container
+
+# Each phoneme needs an ID, Text of it's phonetic pronounciation, an example, a sound, and an animation.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- src/types/phomeme.gd
+++ src/types/phomeme.gd
@@ -2,12 +2,22 @@ extends Node
 class_name Phoneme
 
 var id : int = -1;
-
+var letter_representation : String = "N/A"
+var example_word : String = "NONE"
+var sound_clip : AudioStreamMP3 = null
+var sprite_frames : SpriteFrames = null
+var frames : Array[Texture2D]
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	create_frames()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func create_frames():
+	sprite_frames.add_animation("FRAMES")
+	for i in range(0,frames.size()):
+		sprite_frames.add_frame("FRAMES", frames[i])
