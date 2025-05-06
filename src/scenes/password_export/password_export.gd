d1b403418c80812617ce105dd65ff6cb451ff595
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-05-06 14:37:55 -0700

    Started framework for local play

--- src/scenes/password_export/password_export.gd
+++ src/scenes/password_export/password_export.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var password_holder : TextEdit
 var main_menu_scene : String = "uid://cxg5bb4sk08en"
+var word_guesser_scene : String = "uid://dt671ex55ahu4"
+
 func _ready():
 	encode_password()
 
@@ -18,9 +20,18 @@ func encode_password():
 	print(password_holder.text)
 
 
-
+#region Scene Transitions
 func back_to_main_menu():
 	get_tree().change_scene_to_file(main_menu_scene)
 
 func _on_menu_pressed():
 	call_deferred("back_to_main_menu")
+
+func to_word_guesser():
+	get_tree().change_scene_to_file(word_guesser_scene)
+	
+func _on_play_local_pressed() -> void:
+	Globals.decoded_built_word = Globals.player_built_word
+	Globals.decoded_typed_word = Globals.player_typed_word
+	call_deferred("to_word_guesser")
+#endregion
