a2142d453577c47cccf75a05fab8b336470964c7
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-05-12 17:06:52 -0700

    Regions, hear selected guess button functional

--- src/scenes/word_guesser/word_guesser.gd
+++ src/scenes/word_guesser/word_guesser.gd
@@ -12,9 +12,10 @@ var mystery_guess : PackedScene = preload("res://prefabs/phoneme_selection/phone
 @export var selected_letters : Label
 @export var selected_example : Label
 @export var phoneme_container : HFlowContainer
+@export var speaker : AudioStreamPlayer
 
+var debug := true
 
-var debug := true;
 var guesses : int = 3
 const NO_SELECTION : int = -1
 var current_selection :  int = NO_SELECTION
@@ -25,7 +26,7 @@ var correct_guesses : Array[int]
 
 func _ready() -> void:
 	for child in phoneme_container.get_children():
-		child.added.connect(_on_selection_added)
+		child.added.connect(_on_selection_chosen)
 	if(debug):
 		Globals.decoded_built_word = [5,24,6]
 		Globals.decoded_typed_word = "JACK"
@@ -33,12 +34,10 @@ func _ready() -> void:
 	count_letters()
 	# Initialize guess count
 	guesses_changed()
-	# Initialize phoneme selection window
-	phoneme_selected()
 	# Initialize guess container with mystery guesses
 	initialize_guess_container()
 
-
+#region Initialization
 func count_letters():
 	num_letters.text = ""
 	for letter in Globals.decoded_typed_word.length():
@@ -46,12 +45,14 @@ func count_letters():
 	if(num_letters.text.length() > 0):
 		num_letters.text.erase(num_letters.text.length()-1,1)
 
-func guesses_changed():
-	if guesses == 0:
-		print("GAME OVER")
-	else:
-		num_guesses.text = "INCORRECT GUESSES LEFT: " + str(guesses)
+func initialize_guess_container():
+	for sound in Globals.decoded_built_word:
+		var mystery_sound = mystery_guess.instantiate()
+		mystery_sound.is_guess = true
+		guess_container.add_child(mystery_sound)
+#endregion
 
+#region Phoneme Selection
 func phoneme_selected():
 	if current_selection != NO_SELECTION:
 		# Assign sound, frames and text on click
@@ -59,9 +60,17 @@ func phoneme_selected():
 		selected_example.text = Libraries.word_lib[current_selection]
 		selected_sound = Libraries.sound_lib[current_selection]
 
-func _on_selection_added(id):
+func _on_selection_chosen(id):
 	current_selection = id
 	phoneme_selected()
+#endregion
+
+#region Guess Management
+func guesses_changed():
+	if guesses == 0:
+		print("GAME OVER")
+	else:
+		num_guesses.text = "INCORRECT GUESSES LEFT: " + str(guesses)
 
 func guess(guess_id : int):
 	#TODO: Handle guessing a correct guess multiple times? Does it matter?
@@ -75,14 +84,11 @@ func guess(guess_id : int):
 		guesses -= 1
 		guesses_changed()
 
-func initialize_guess_container():
-	for sound in Globals.decoded_built_word:
-		var mystery_sound = mystery_guess.instantiate()
-		mystery_sound.is_guess = true
-		guess_container.add_child(mystery_sound)
-	print(guess_container.get_children())
-
-
 func _on_guess_sound_pressed() -> void:
 	if current_selection != NO_SELECTION:
 		guess(current_selection)
+#endregion
+
+func _on_hear_sound_pressed() -> void:
+	speaker.stream = selected_sound
+	speaker.play()
