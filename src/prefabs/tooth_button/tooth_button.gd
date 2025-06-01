c5a79d0a5e173adb81a0ac416f80ef7204283158
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-05-31 17:24:37 -0700

    architecture for tooth button

--- src/prefabs/tooth_button/tooth_button.gd
+++ src/prefabs/tooth_button/tooth_button.gd
@@ -1,20 +1,22 @@
 extends Node2D
 @export var anim : AnimatedSprite2D
-@export var button : Button
 var mouse_hovering : bool = false
-# Called when the node enters the scene tree for the first time.
+@export var label : Label
+var on_click : Callable = debug_click
+
+
+# To prevent animation interuption, animations can only change when one loops or finishes
 func _ready():
-	button.visible = false
 	anim.play()
+	label.visible = false
 
 func _on_area_2d_mouse_entered():
 	mouse_hovering = true
-	pass # Replace with function body.
 
 func _on_area_2d_mouse_exited():
 	mouse_hovering = false
 	anim.animation = "retract"
-	button.visible = false
+	label.visible = false
 	anim.play()
 
 func _on_animated_sprite_2d_animation_finished():
@@ -22,14 +24,19 @@ func _on_animated_sprite_2d_animation_finished():
 		anim.animation = "default"
 		anim.play()
 	elif anim.animation == "extend":
-		button.visible = true
-		pass
+		label.visible = true
 
 func _on_animated_sprite_2d_animation_looped():
 	if mouse_hovering == true:
 		anim.animation = "extend"
 		anim.play()
 
-func _on_button_pressed():
-	print("PRESSED")
-	pass # Replace with function body.
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if(event is InputEventMouseButton and event.button_index == 1 and event.pressed == true):
+		on_click.call()
+	else:
+		label.text = "PRESS"
+
+func debug_click():
+	print("YA CLICKED ME")
+	label.text = "CLICKY"
